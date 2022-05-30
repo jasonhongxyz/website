100a69e4f0684c206a319726ac71864ca417566f
Jason Hong <jasonhong0810@gmail.com>
2022-05-30 10:37:57 -0500

    adding _site

---
+++ _site/docker-push.sh
@@ -0,0 +1,3 @@
+jekyll build
+docker buildx build -f Dockerfile-jenkins --platform linux/amd64,linux/arm64 --push -t jasonhongxyz/myjenkins:latest .
+docker buildx build -f Dockerfile-blog --platform linux/amd64,linux/arm64 --push -t jasonhongxyz/blog:latest .
