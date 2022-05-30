8b3c62fa97c4db50f509867117b4053e7fdd15c1
Jason Hong <jasonhong0810@gmail.com>
2022-05-30 10:05:30 -0500

    testing jenkins

--- docker-push.sh
+++ docker-push.sh
@@ -1,2 +1,3 @@
-docker buildx build --platform linux/amd64,linux/arm64 --push -t jasonhongxyz/myjenkins:latest ./jenkins
-docker buildx build --platform linux/amd64,linux/arm64 --push -t jasonhongxyz/blog:latest .
+jekyll build
+docker buildx build -f Dockerfile-jenkins --platform linux/amd64,linux/arm64 --push -t jasonhongxyz/myjenkins:latest .
+docker buildx build -f Dockerfile-blog --platform linux/amd64,linux/arm64 --push -t jasonhongxyz/blog:latest .
