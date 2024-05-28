4e540f947401e559781485e56c81f5a9b43154df
Jason Hong <jasonhong0810@gmail.com>
2024-05-28 18:39:28 -0500

    Update site description

--- jsnhongcom/src/site.config.ts
+++ jsnhongcom/src/site.config.ts
@@ -18,7 +18,7 @@ export const siteConfig: SiteConfig = {
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Jason Hong | :)",
 	// Meta property used as a default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "I must not fear. Fear is the mind-killer.",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
